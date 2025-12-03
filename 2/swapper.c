546481fb003d44f96619ffb676f9f3edb800a843
patryk100 <pperkowski2@gmail.com>
2025-12-03 07:08:39 -0500

    deal with bug for odd num of elements in arr

--- 2/swapper.c
+++ 2/swapper.c
@@ -8,13 +8,13 @@ void inplace_swap(int *x, int *y) {
 
 void reverse_arr(int a[], int cnt) {
   int first, last;
-  for (first = 0, last = cnt - 1; first <= last; first++, last--)
+  for (first = 0, last = cnt - 1; first < last; first++, last--)
     inplace_swap(&a[first], &a[last]);
 }
 
 int main() {
-  int numbers[] = {25, 50, 75, 100};
-  reverse_arr(numbers, 4);
+  int numbers[] = {25, 50, 75, 100, 125};
+  reverse_arr(numbers, sizeof(numbers) / sizeof(numbers[0]));
   printf("%d ", numbers[0]);
   printf("%d \n", numbers[1]);
   return 0;
